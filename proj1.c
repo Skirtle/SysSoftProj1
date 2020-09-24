9ceb937e33a9f58ac6688900e1f42f0dd7872409
Skirtle <53882475+Skirtle@users.noreply.github.com>
2020-09-23 22:55:05 -0400

    Update proj1.c

--- proj1.c
+++ proj1.c
@@ -15,9 +15,26 @@ typedef struct {
     int m; // M
 } Instruction;
 
+// Prototypes
+Instruction* getInstructionFromFile(FILE* file, int *len);
 
-int main() {
-    // Main
+int main(int argc, char *argv[]) {
+    /* Helpful information
+    * argv[0]: .exe location
+    * argv[1]: 1st actual argument (should be file name)
+    */
+
+   char* filename = argv[1];
+   FILE* ipf = fopen(filename, "r"); //Opens the command line text file given
+
+    if (ipf == NULL) { //If the file name is wrong, or file does not exist, return 1 and exit
+        printf("Filename \"%s\" not valid. FILE* ipf is NULL", filename);
+        return 1;
+    }
 
     return 0;
+}
+
+Instruction* getInstructionFromFile(FILE* file, int *len) {
+
 }
